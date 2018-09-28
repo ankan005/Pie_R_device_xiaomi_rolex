e27ea849f76e89b75060a0fece5882406950fe37
Ankan005 <ankan005@gmail.com>
2018-09-28 12:34:19 +0000

    bringup_Havoc

--- havoc_rolex.mk
+++ havoc_rolex.mk
@@ -17,9 +17,13 @@
 $(call inherit-product, device/xiaomi/rolex/full_rolex.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/products/common.mk)
 
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := havoc_rolex
+PRODUCT_DEVICE := rolex
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 4A
+PRODUCT_NAME := havoc_rolex
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
